b0da34665f24d3f5ccfd4cfe0499ed0f80c5226c
satanas <wil.alejandro@gmail.com>
2018-07-01 00:04:33 -0700

    Added responsive title

--- games/tomatromp/js/main.js
+++ games/tomatromp/js/main.js
@@ -1,9 +1,11 @@
-var $hero, videoContainer, $download;
+var $hero, videoContainer, $download, $title;
 
 window.addEventListener('load', () => {
   $hero = document.getElementsByClassName('hero')[0];
   $videoContainer = document.getElementsByClassName('video-container')[0];
   $download = document.getElementsByClassName('downloads')[0];
+  $title = document.getElementsByClassName('title')[0];
+
   resize();
 });
 
@@ -15,9 +17,11 @@ function resize() {
   var width = document.body.clientWidth;
   var heroHeight = parseInt(width / 1.966);
   var videoContainerHeight = parseInt((width - (width * 0.20))/ 1.77);
-  var downloadPadding = parseInt(width / 3.695);
+  var downloadPadding = parseInt(width / 25.714);
+  var titlePadding = parseInt(width / 18);
 
   $hero.style.height = heroHeight + 'px';
   $videoContainer.style.height = videoContainerHeight + 'px';
   $download.style.paddingTop = downloadPadding + 'px';
+  $title.style.paddingTop = titlePadding + 'px';
 }
