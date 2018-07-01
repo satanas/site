0e78dab0b2477baa86de6356292982796857afce
satanas <wil.alejandro@gmail.com>
2018-06-30 19:46:56 -0700

    Responsive download badges

--- games/tomatromp/js/main.js
+++ games/tomatromp/js/main.js
@@ -1,8 +1,9 @@
-var $hero, videoContainer;
+var $hero, videoContainer, $download;
 
 window.addEventListener('load', () => {
   $hero = document.getElementsByClassName('hero')[0];
   $videoContainer = document.getElementsByClassName('video-container')[0];
+  $download = document.getElementsByClassName('downloads')[0];
   resize();
 });
 
@@ -14,7 +15,9 @@ function resize() {
   var width = document.body.clientWidth;
   var heroHeight = parseInt(width / 1.966);
   var videoContainerHeight = parseInt((width - (width * 0.20))/ 1.77);
+  var downloadPadding = parseInt(width / 3.695);
 
   $hero.style.height = heroHeight + 'px';
   $videoContainer.style.height = videoContainerHeight + 'px';
+  $download.style.paddingTop = downloadPadding + 'px';
 }
